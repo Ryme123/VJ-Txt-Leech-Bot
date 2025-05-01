7807a857fa5cf65d45569559f6dc979f0620652d
Ryme123 <premium742159@gmail.com>
2025-05-01 12:17:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import modify_link
 
-if name == "main":
+if __name__ == "__main__":
     # Example usage
     original_link = input("Enter the original link: ").strip()
     modified_link = modify_link(original_link)
